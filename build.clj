81e2ac05ecbf0bfefaef23208da3b69f45b46def
James Reeves <jreeves@weavejester.com>
2025-09-16 23:25:20 +0100

    Add more keywords to the build docs script

--- build.clj
+++ build.clj
@@ -9,8 +9,14 @@
       (str/replace #"(\n[^-][^\n]*)\n-" "$1\n\n-")))
 
 (def keywords
-  [:duct.logger/simple
-   :duct.module/logging])
+  [:duct.database/sql
+   :duct.database.sql/hikaricp
+   :duct.logger/simple
+   :duct.module/cljs
+   :duct.module/logging
+   :duct.module/sql
+   :duct.module/web
+   :duct.server.http/jetty])
 
 (ig/load-annotations)
 
