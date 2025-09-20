8855bea438f88000a26b0ce2fc72d73ca100f15a
James Reeves <jreeves@weavejester.com>
2025-09-20 17:42:10 +0100

    Add dependency info to component directory

--- build.clj
+++ build.clj
@@ -35,6 +35,24 @@
    :duct.router/reitit
    :duct.server.http/jetty])
 
+(defn- namespace->path [kw]
+  (-> (namespace kw) (str/replace #"\." "/") (str/replace #"-" "_")))
+
+(defn- keyword->path [kw]
+  (str (namespace->path kw) "/" (-> (name kw) (str/replace #"-" "_"))))
+
+(defn- find-resource [kw]
+  (or (io/resource (str (keyword->path kw) ".clj"))
+      (io/resource (str (namespace->path kw) ".clj"))))
+
+(def ^:private re-artifact-path
+  #"\/\.m2/repository/(.*)/(.*?)/(.*?)/(.*?)\.jar\!")
+
+(defn- get-dependency [path]
+  (let [[_ group artifact version _] (re-find re-artifact-path (str path))]
+    (str (str/replace group #"/" ".") "/" artifact
+         " {:mvn/version \"" version "\"}")))
+
 (ig/load-annotations)
 
 (with-open [writer (io/writer "keywords.adoc")]
@@ -42,6 +60,10 @@
     (let [doc (-> kw ig/describe :doc trim-indent space-out-lists ->asciidoc)]
       (binding [*out* writer]
         (println "###" kw)
+        (newline)
+        (println "[sidebar]")
+        (println (str "*dependency:* `"
+                      (get-dependency (find-resource kw)) "`"))
         (newline)
         (println doc)))))
 
